d9bddffee4d406bda9d52c389e87b23572644037
Morgan Thibert <morgan.thibert@gmail.com>
2023-10-25 14:42:47 +0200

    refactor to one branch

--- screenlayout/.screenlayout/layout.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-0 --primary --mode 3840x2160 --pos 2560x664 --rotate normal --output DP-0 --mode 1280x1024 --pos 1280x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 6400x1204 --rotate normal --output DP-4 --mode 2560x1440 --pos 0x1024 --rotate normal --output DP-5 --off
