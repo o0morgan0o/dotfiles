f09bdd86ab77cdb62918f30d4b0cca24d249cb44
o0morgan0o <morgan.thibert@gmail.com>
2023-10-25 22:35:07 +0200

    experiment

--- util-scripts/.config/scripts/i3-tmux.sh
+++ util-scripts/.config/scripts/i3-tmux.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #i3-msg focus "right"
+alacritty --config-file ~/.config/alacritty/alacritty.toml
 
-if ! tmux has-session -t "main" 2> /dev/null; then
-  tmux new-session -s "main" -c "main" -d
-  alacritty -e tmux
-fi
+#if ! tmux has-session -t "main" 2>/dev/null; then
+#  tmux new-session -s "main" -c "main" -d
+#  alacritty --config-file ~/.config/alacritty/alacritty.toml
+#fi
 
 #i3-sensible-terminal
 #alacritty
@@ -13,7 +14,7 @@ fi
 #alacritty -e "tmux switch-client -t \"main\""
 #alacritty -e 'tmux attach-session -t "main"; zsh'
 
-tmux split
-i3-msg [title=tmux] split
-i3-msg [title=Alacritty] focus
+#tmux split
+#i3-msg [title=tmux] splialacrit
+#i3-msg [title=Alacritty] focus
 #tmux switch-client -t "main"
