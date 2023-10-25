f9209edefe75255a8f8a2bc32ad342a413b6ada1
Morgan Thibert <morgan.thibert@gmail.com>
2023-10-26 01:24:24 +0200

    minor tweaks

--- util-scripts/.config/scripts/i3-tmux.sh
+++ util-scripts/.config/scripts/i3-tmux.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 #i3-msg focus "right"
-alacritty --config-file=$HOME/.config/alacritty/alacritty.toml
+#alacritty --config-file=$HOME/.config/alacritty/alacritty.toml
 
-#if ! tmux has-session -t "main" 2>/dev/null; then
-#  tmux new-session -s "main" -c "main" -d
-#  alacritty --config-file ~/.config/alacritty/alacritty.toml
-#fi
+if ! tmux has-session -t "main" 2>/dev/null; then
+  tmux new-session -s "main" -c "main" -d
+  alacritty --config-file=$HOME/.config/alacritty/alacritty.toml -e $SHELL -c 'tmux attach -t "main" || tmux'
+  #alacritty --config-file ~/.config/alacritty/alacritty.toml
+  #alacritty --config-file ~/.config/alacritty/alacritty.toml
+else
 
 #i3-sensible-terminal
 #alacritty
 #/bin/bash
 #alacritty -e "tmux switch-client -t \"main\""
 #alacritty -e 'tmux attach-session -t "main"; zsh'
+alacritty --config-file=$HOME/.config/alacritty/alacritty.toml -e $SHELL -c 'tmux switch-client -t "main" && tmux split-window -h'
+#alacritty --config-file=$HOME/.config/alacritty/alacritty.toml -e $SHELL -c 'tmux attach -t "main" && tmux split-window -h'
 
+#alacritty --config-file=$HOME/.config/alacritty/alacritty.toml -e $SHELL -c 'tmux split-pane -h -t ["main":]4'
 #tmux split
 #i3-msg [title=tmux] splialacrit
 #i3-msg [title=Alacritty] focus
 #tmux switch-client -t "main"
+fi
