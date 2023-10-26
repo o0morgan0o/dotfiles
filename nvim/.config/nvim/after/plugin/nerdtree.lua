4099486ea62a5988b52b73e27fc56307ba25b984
o0morgan0o <morgan.thibert@gmail.com>
2023-10-26 04:31:55 +0200

    experiment

--- nvim/.config/nvim/after/plugin/nerdtree.lua
+++ nvim/.config/nvim/after/plugin/nerdtree.lua
@@ -1,2 +1,3 @@
-vim.keymap.set("n", "<M-1>", vim.cmd.NERDTreeToggle)
+--vim.keymap.set("n", "<M-1>", vim.cmd.NERDTreeToggle)
+vim.api.nvim_set_keymap("n", "<M-1>", "<Cmd>:NERDTreeToggle<CR>", {noremap=true})
 
