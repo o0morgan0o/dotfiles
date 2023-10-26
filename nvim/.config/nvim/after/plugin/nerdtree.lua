bee8cd62413fa9fa82cd74848cd63ae2a49a76ee
Morgan Thibert <morgan.thibert@gmail.com>
2023-10-26 04:06:56 +0200

    add neovim config

---
+++ nvim/.config/nvim/after/plugin/nerdtree.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<M-1>", vim.cmd.NERDTreeToggle)
+
---
+++ nvim/.config/nvim/lua/personal/remap.lua
@@ -0,0 +1,6 @@
+local defaults = { noremap = true, silent = true }
+-- vim.keymap.set("i", "jk", "<esc>", defaults)
+vim.cmd("inoremap <nowait> jk <Esc>")
+
+
+
