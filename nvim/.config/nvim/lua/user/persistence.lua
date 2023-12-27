71d2d9c57fdf86b07422be4020ae823bc1f450e8
Morgan Thibert <morgan.thibert@gmail.com>
2023-12-27 14:23:33 +0100

    feat: ajout copilot

---
+++ nvim/.config/nvim/lua/user/persistence.lua
@@ -0,0 +1,23 @@
+local M = {
+	"folke/persistence.nvim",
+	event = "BufReadPre",
+	opts = {
+		--	dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"), -- directory where session files are saved
+		--	options = { "buffers", "curdir", "tabpages", "winsize" }, -- sessionoptions used for saving
+		--	pre_save = nil, -- a function to call before saving the session
+		--	save_empty = false, -- don't save if there are no open file buffers
+	},
+}
+
+function M.config() end
+
+-- restore the session for the current directory
+vim.api.nvim_set_keymap("n", "<leader>qs", [[<cmd>lua require("persistence").load()<cr>]], {})
+
+-- restore the last session
+vim.api.nvim_set_keymap("n", "<leader>ql", [[<cmd>lua require("persistence").load({ last = true })<cr>]], {})
+
+-- stop Persistence => session won't be saved on exit
+vim.api.nvim_set_keymap("n", "<leader>qd", [[<cmd>lua require("persistence").stop()<cr>]], {})
+
+return M
