be7d4dc100420931b1d02064720585c2b28e1de9
o0morgan0o <morgan.thibert@gmail.com>
2024-05-22 00:11:43 +0200

    fix: shebang

--- shared/polybar/.config/polybar/scripts/docker-module.sh
+++ shared/polybar/.config/polybar/scripts/docker-module.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ $(docker ps -q | wc -c ) -gt 0 ]];
-	then docker ps --format '{{.Names}} {{.ID}} {{.Ports}}' \
-| sed 's/,\ //g' \
-| sed 's/0\.0\.0\.0://g' \
-| awk '
+if [[ $(docker ps -q | wc -c) -gt 0 ]]; then
+	docker ps --format '{{.Names}} {{.ID}} {{.Ports}}' |
+		sed 's/,\ //g' |
+		sed 's/0\.0\.0\.0://g' |
+		awk '
 { $2 = substr($2,1,3)}
 {print "%{B#6300a9}%{F#fff}"}
 {print $2} 
@@ -14,12 +14,13 @@ if [[ $(docker ps -q | wc -c ) -gt 0 ]];
 {print $3}
 {print "%{T-}%{B-}%{F-}"}
 {print "||"} 
-' \
-| tr '\n' ' ' \
-| sed 's/\/tcp/\ /g' \
-| sed 's/[[:space:]]\:\:\:[0-9]*->[0-9]*[[:space:]]/\ /g' \
-| tr -s ' ' \
-| sed 's/||[[:space:]]$//' \
-;
-	else echo "no container...";
+' |
+		tr '\n' ' ' |
+		sed 's/\/tcp/\ /g' |
+		sed 's/[[:space:]]\:\:\:[0-9]*->[0-9]*[[:space:]]/\ /g' |
+		tr -s ' ' |
+		sed 's/||[[:space:]]$//' \
+		;
+else
+	echo "no container..."
 fi
