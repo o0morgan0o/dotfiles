92344431beda7cc6ac331c57730fab778c31b3e0
o0morgan0o <morgan.thibert@gmail.com>
2024-06-05 04:27:12 +0200

    chore: misc

--- shared/fish/.config/fish/config.fish
+++ shared/fish/.config/fish/config.fish
@@ -1,4 +1,9 @@
+export NNN_PLUG='f:finder;o:fzopen;p:preview-tui;d:diffs;t:preview-tabbed;v:imgview'
+export NNN_FCOLORS='c1e2272e006033f7c6d6abc4'	
+
 set fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set --export NNN_FIFO "/tmp/nnn.fifo"
