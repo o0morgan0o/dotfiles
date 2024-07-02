3804d3ccda7740217343cb8f2f4e28a7c05edc1f
o0morgan0o <morgan.thibert@gmail.com>
2024-07-02 20:03:27 +0200

    chore: update for new screen

--- desktop/util-scripts/.config/scripts/layout.sh
+++ desktop/util-scripts/.config/scripts/layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --primary --mode 3840x2160 --pos 3840x1024 --rotate normal --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 7680x1024 --rotate normal --output DP-2 --mode 1280x1024 --pos 2465x0 --rotate normal --output DP-3 --off --output DP-4 --mode 3840x2160 --pos 0x1024 --rotate normal --output DP-5 --off
+xrandr --output HDMI-0 --primary --mode 3840x2160 --pos 3840x1080 --rotate normal --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 7680x1080 --rotate normal --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-4 --mode 3840x2160 --pos 0x1080 --rotate normal --output DP-5 --off
